14b6451020c09135857ca819eee5b411bf73539e
sammymans <sfarnum3@uwo.ca>
2023-01-06 16:20:44 -0500

    young phys

---
+++ a2oj ladders/ladder 11/Young Physicist/A.py
@@ -0,0 +1,13 @@
+n = int(input())
+# print(n)
+x, y, z = 0, 0, 0
+for i in range(n):
+    arr = list(map(int,input().split()))
+    x += arr[0]
+    y += arr[1]
+    z += arr[2]
+
+if x == 0 and y == 0 and z == 0:
+    print("YES")
+else:
+    print("NO")
